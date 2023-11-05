06a5ffc06e01652bf46b40b45a76cbd83af56fc4
Nhat Trieu <vntrieu25879@gmail.com>
2023-11-05 20:03:25 +0700

    Make Tailwind work

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+// import userReducer from './slices/user.slice'
+// import gamePlayReducer from './slices/user.slice'
+
+
+// const reducer = combineReducers({
+//     user: userReducer,
+// });
+// export default configureStore({
+//     reducer
+// })
